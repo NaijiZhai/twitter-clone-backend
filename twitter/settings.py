d262c0d630f8b5014c68a57e5e5d4645a7c6a0d8
zhainaiji <zhai.n@northeastern.edu>
2025-07-18 20:12:45 -0400

    update-settings

--- twitter/settings.py
+++ twitter/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['twitter.naijizhai.com']
 # INTERNAL_IPS = ['10.0.2.2']
 
 # Application definition
